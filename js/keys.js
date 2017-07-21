bdc2565723266610e43abfbda23abcee94f36fac
mccarthinator <mccarthy.cath@gmail.com>
2017-07-21 15:13:22 -0500

    updating access keys and tokens

--- js/keys.js
+++ js/keys.js
@@ -1,8 +1,8 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: 'bej8KqH1xRaH9qEjxFsI2R7F7',
-  consumer_secret: 'ZLjvJR5GONQ7XetYQJUC0vyNxV48tHH6wnAlWzyDORBXZlGDA3',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
-}
+  consumer_key: 'bAJrcVKs5QpXSodjkLQ9H8P07',
+  consumer_secret: 'DkGxjBaM244SkICCBWAGVVTsjhTolMzAcOLhu0fTkXTIvufCmy',
+  access_token_key: '1966990682-7gByXBDIJWc56XdJGsKl1b4x3qtXcSJh7hgSTwU',
+  access_token_secret: '4D1CE0KvDLsgmxAi79ICFOizp9ghf8rdVD62lLnBCDI7L',
+} 
