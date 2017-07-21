d45b8f8d3bc9385f3d7d3c763761b99896b88dce
mccarthinator <mccarthy.cath@gmail.com>
2017-07-21 15:09:13 -0500

    initializing files

---
+++ js/keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'bej8KqH1xRaH9qEjxFsI2R7F7',
+  consumer_secret: 'ZLjvJR5GONQ7XetYQJUC0vyNxV48tHH6wnAlWzyDORBXZlGDA3',
+  access_token_key: '<input here>',
+  access_token_secret: '<input here>',
+}
---
+++ js/liri.js
@@ -0,0 +1,9 @@
+//code for importing data from keys.js file goes here. require?
+
+//then store keys data in a var 
+
+// //Make it so liri.js can take in one of the following commands:
+// my-tweets
+// spotify-this-song
+// movie-this
+// do-what-it-says
