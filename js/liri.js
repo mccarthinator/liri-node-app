8526394419f5ac82eef2b2464e721d5c8ac3fc0a
mccarthinator <mccarthy.cath@gmail.com>
2017-07-21 15:25:44 -0500

    linked keys js and liri js

--- js/liri.js
+++ js/liri.js
@@ -1,9 +1,14 @@
 //code for importing data from keys.js file goes here. require?
+var twitterKeys = require("keys.js");
 
-//then store keys data in a var 
+//then store keys data in a var ^
 
 // //Make it so liri.js can take in one of the following commands:
 // my-tweets
 // spotify-this-song
 // movie-this
 // do-what-it-says
+
+function mytweets () {
+    
+}
