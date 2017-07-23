0224deb6a8dbefb343f01c97c8b4d3fb93e0dca8
mccarthinator <mccarthy.cath@gmail.com>
2017-07-22 22:51:28 -0500

    updating keys.js

--- js/keys.js
+++ js/keys.js
@@ -6,3 +6,10 @@ exports.twitterKeys = {
   access_token_key: '1966990682-7gByXBDIJWc56XdJGsKl1b4x3qtXcSJh7hgSTwU',
   access_token_secret: '4D1CE0KvDLsgmxAi79ICFOizp9ghf8rdVD62lLnBCDI7L',
 } 
+
+var Spotify = require('node-spotify-api');
+ 
+exports.spotifyKeys = new Spotify({
+  id: '2fde161bf188437782eb8227883e2324',
+  secret: '07e43665ab4f4239aaeffbb2e1a3d18f'
+});
